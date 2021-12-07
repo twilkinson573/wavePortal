c52e822d0a4409682109da81c137a53553dab067
tuds <twilkinson573@gmail.com>
2021-12-07 15:19:34 +0000

    Add initial WavePortal getter/setter methods & dev script

---
+++ scripts/run.js
@@ -0,0 +1,29 @@
+const main = async () => {
+  const [owner, randoPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn;
+  waveTxn = await waveContract.wave();
+
+  waveCount = await waveContract.getTotalWaves();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
